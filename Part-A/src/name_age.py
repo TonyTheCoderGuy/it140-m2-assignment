6ff9110e81365113f29bc0e63d34496d62597bf2
TonyTheCoderGuy <326190579+TonyTheCoderGuy@users.noreply.github.com>
2026-09-12 20:35:37 +0000

    Complete Part A name and age program

--- Part-A/src/name_age.py
+++ Part-A/src/name_age.py
@@ -9,13 +9,14 @@ Process:
     TODO: Replace with a major processing step.
 
 Output:
-    TODO: Replace with a major output, including its type and destination.
+    ODO:TODO: Replace with a major output, including its type and destination.
 
 Typical usage example:
     TODO: Replace with the input prompt and original name-input example.
     TODO: Replace with the input prompt and original age-input example.
     TODO: Replace with the resulting output from those inputs.
 """
+
 # === Imports ===
 from datetime import date
 
@@ -29,14 +30,13 @@ def main() -> None:
     """Run the name-age program."""
 
     # Get user input.
-    # TODO: Replace with code to get user's name as a string. See zyBooks 1.3.
-    # TODO: Replace with code to get user's age as an integer. See zyBooks 2.6.
+    user_name = input("What is your name? ")
+    user_age = int(input("How old are you? "))
 
-    # Calculate user's approximate birth year.
-    # TODO: Replace with code to process data. See zyBooks 1.16 & 1.17.
+    birth_year = CURRENT_YEAR - user_age
 
-    # Output personalized message with user's name and birth year.
-    # TODO: Replace with code to output formatted results. zyBooks 1.3 & 2.7.
+    print()
+    print(f"Hello {user_name}! You were born in {birth_year}.")
 
 
 # === Main Guard ===
